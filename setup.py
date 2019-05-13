6a2d8bbeabf876c96d95f7936921583b609dde40
diegoarabcohen <diego.arab.cohen@gmail.com>
2019-05-13 14:57:48 +0300

    test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='testpackage',
       version='0.1',
       description='The first package test',
-      url='https://gist.github.com/diegoarabcohen/9cf9b51bc736379c87e3699e82292d1d',
+      url='https://github.com/diegoarabcohen/testpackage',
       author='Diego Arab Cohen',
       author_email='diego.arab.cohen@gmail.com',
       license='MIT',
